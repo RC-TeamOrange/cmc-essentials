cd16f00e6eacf95fdf21349bde75779624f552b0
Ketoma Kemanji <ketvixk@gmail.com>
2016-04-10 22:03:44 +0100

    Initial commit Laravel files.

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace CmcEssentials\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CmcEssentials\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
