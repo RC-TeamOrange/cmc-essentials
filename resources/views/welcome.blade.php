607726b166a0f25498ed261d2211a54cd4676ed8
Simona <otelea.simona@yahoo.com>
2016-04-17 17:37:31 +0300

    Added content to welcome and syllabus pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,9 @@
 <h2>About CMC-Essentials</h2>
 @stop
 @section('content')
-<p>This is a web app for introductory studies into CMC.</p>
+
+<p>The website is intended to help you learn about Computer-Mediated-Communication (CMC).</p>
+
 <a href="{{ route('syllabus') }}">Continue</a>
+
 @stop
