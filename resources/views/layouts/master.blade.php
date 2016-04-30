09d0396f337b22bb0058aa1c8198a7116def7ea6
Ketoma Kemanji <ketvixk@gmail.com>
2016-04-30 08:09:45 +0100

    Added bootsrap core js file.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,6 +12,7 @@
         <link rel="stylesheet" href="{{ asset('css/cmc-essentials.css') }}">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
         <script src="{{ asset('js/cmc-essentials.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     </head>
     
     <body>
